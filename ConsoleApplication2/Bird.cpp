6fc8230245e3507762bc7d9577f2f62690acc35e
BartlomiejSzozda <bartekszozda19@gmail.com>
2018-08-19 19:47:38 +0200

    this files should be added 2 commits ago

---
+++ ConsoleApplication2/Bird.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "Bird.h"
+
+void Bird::updatePosition(float elapsedTime) {
+	if (imAlive) {
+		velocity += acceleration * elapsedTime;
+		position += velocity * elapsedTime;
+	}
+}
+
+bool Bird::checkIfBirdHasMovedFromLastCheck(float levelPosition) {
+	if ((int)levelPosition != (int)lastLevelPosition) {
+		lastLevelPosition = levelPosition;
+		return true;
+	}
+	return false;
+}
+
+void Bird::flap() {
+	if (imAlive) {
+		velocity = -50.0f + (rand() % 2);
+		++flapCount;
+	}
+}
+
+void Bird::reborn(float pos, float vel) {
+	if (!imAlive) {
+		imAlive = true;
+		position = pos;
+		velocity = vel;
+		flapCount = 0;
+		fitnessPoint = 0;
+	}
+}
+
+void Bird::die() {
+	imAlive = false;
+	if (flapCount > maxFlapCount) {
+		maxFlapCount = flapCount;
+	}
+	if (fitnessPoint > maxfitnessPoint) {
+		maxfitnessPoint = fitnessPoint;
+	}
+}
+
---
+++ ConsoleApplication2/BirdsGroup.cpp
@@ -0,0 +1,57 @@
+#pragma once
+#include "BirdsGroup.h"
+
+BirdsGroup::BirdsGroup(int numOfBirds, float pos, float vel, float acc) {
+	srand(time(NULL));
+	birds = std::vector<Bird>(numOfBirds, Bird(pos, vel, acc));
+	numberOfBirds = numberOfLivingBirds = numOfBirds;
+}
+
+void BirdsGroup::updatePosition(float elapsedTime) {
+	for (Bird& b : birds) {
+		b.updatePosition(elapsedTime);
+	}
+}
+
+void BirdsGroup::flap() {
+	for (Bird& b : birds) {
+		b.flap();
+	}
+}
+
+void BirdsGroup::reborn(float pos, float vel) {
+	numberOfLivingBirds = numberOfBirds;
+	for (Bird& b : birds) {
+		b.reborn(pos, vel);
+	}
+}
+
+void BirdsGroup::birdDie(int birdNumber) {
+	if (birds[birdNumber].getFlapCount() > maxBestFlapCount) {
+		maxBestFlapCount = birds[birdNumber].getFlapCount();
+	}
+	if (birds[birdNumber].getFitnessPoint() > maxBestFitnessPoint) {
+		maxBestFitnessPoint = birds[birdNumber].getFitnessPoint();
+	}
+	birds[birdNumber].die();
+	if (numberOfLivingBirds > 0) {
+		--numberOfLivingBirds;
+	}
+	else
+		throw exception();
+}
+
+int BirdsGroup::getBestFlapCount() {
+	for (Bird& b : birds) {
+		if (b.isAlive())
+			return b.getFlapCount();
+	}
+}
+
+int BirdsGroup::getBestFitnessPoint() {
+	for (Bird& b : birds) {
+		if (b.isAlive())
+			return b.getFitnessPoint();
+	}
+	return 0;
+}
